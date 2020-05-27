0e1c023637c294713446874350d650b74482882d
Amanda Key <ariaandezrasmom@gmail.com>
2020-05-27 17:00:00 -0500

    completed assessment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     cart: [
       {
         id: shortid.generate(),
-        name: "scarf",
+        name: "Scarf",
         price: "11.50",
         quantity: 1,
       },
@@ -21,7 +21,7 @@ class App extends Component {
       },
       {
         id: shortid.generate(),
-        name: "pants",
+        name: "Pants",
         price: "25.50",
         quantity: 1,
       },
@@ -46,9 +46,18 @@ class App extends Component {
   };
 
   handleDecrementQuantity = (id) => {
-    // Your Code Here!
-    // Do the same as handleIncrementQuantity, but decrement it instead
-    // Remember that you should not decrement below zero!
+    this.setState((state) => {
+      const newList = state.cart.map((item) => {
+        if (item.id === id && item.quantity > 0) {
+          return Object.assign({}, item, { quantity: item.quantity - 1});
+        } else {
+          return item;
+        }
+      });
+      return {
+        cart: newList,
+      };
+    });
   };
 
   handleCheckout = () => {
@@ -67,7 +76,7 @@ class App extends Component {
         <ul className="list">
           {this.state.cart.map((item) => (
             <li key={item.id}>
-              <Product your props here />
+              <Product item={item} onIncrease={this.handleIncrementQuantity} onDecrease={this.handleDecrementQuantity} />
             </li>
           ))}
         </ul>
@@ -75,12 +84,9 @@ class App extends Component {
           <label>
             <input
               type="checkbox"
-              defaultChecked={this.stateIncisOnMailingList}
+              defaultChecked={this.stateIsOnMailingList}
               onChange={(event) =>
-                // Your code here!
-                // Use setState to update the flag in state for the checkbox
-                // Hint: use event.target.checked
-                this.setState()
+                this.setState({ isOnMailingList: event.target.checked})
               }
             />
             Sign me up for the mailing list!
