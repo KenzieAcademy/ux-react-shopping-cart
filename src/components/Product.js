6dbdc98f5ae171497a2a26b394c500a0ef5b2787
Sharon Walters <SHARONMARIE919@GMAIL.COM>
2020-09-13 14:30:44 -0400

    Done

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,10 +2,17 @@ import React from "react";
 import Quantity from "./Quantity";
 
 function Product(props) {
+  const handleIncrementQuantity = () =>
+    props.onIncrementQuantity(props.item.id);
   return (
     <span>
-      Item Name - Item Price -
-      <Quantity your props here />
+      {props.item.name} - ${props.item.price} -
+      <Quantity
+        item={props.item}
+        onIncrementQuantity={props.onIncrementQuantity}
+        onDecrementQuantity={props.onDecrementQuantity}
+
+      />
     </span>
   );
 }
